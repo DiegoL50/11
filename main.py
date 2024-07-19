9df0d959e4df1f6732581d6b0c7fe4f21f4f2faf
Jipxmaster <johnoshirokarita@gmail.com>
2024-07-18 20:42:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,233 +1,10 @@
 
 import grenadier
 from other_class import *
-from abilities import SpinachExplosion
-from abilities import BroccoliShield
+
+from soldat import Soldier as Soldier
 
 #"""
-class Soldier(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
-        pygame.sprite.Sprite.__init__(self)
-        self.alive = True
-        self.char_type = char_type
-        self.speed = speed
-        self.ammo = ammo
-        self.start_ammo = ammo
-        self.shoot_cooldown = 0
-        self.grenades = grenades
-        self.health = 100
-        self.max_health = self.health
-        self.direction = 1
-        self.vel_y = 0
-        self.jump = False
-        self.in_air = True
-        self.flip = False
-        self.animation_list = []
-        self.frame_index = 0
-        self.action = 0
-        self.update_time = pygame.time.get_ticks()
-        # AI specific variables
-        self.move_counter = 0
-        self.vision = pygame.Rect(0, 0, 150, 20)
-        self.idling = False
-        self.idling_counter = 0
-
-        #HABILIDADES
-        self.spinach_explosion = SpinachExplosion()
-        self.broccoli_shield = BroccoliShield()
-
-        # load all images for players
-        animation_types = ['Idle', 'Run', 'Jump', 'Death']
-        for animation in animation_types:
-            # reset temp list of images
-            temp_list = []
-            # count number of files in the folder
-            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
-            for i in range(num_of_frames):
-                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
-                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-                temp_list.append(img)
-            self.animation_list.append(temp_list)
-
-        self.image = self.animation_list[self.action][self.frame_index]
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-
-    def update(self):
-        self.update_animation()
-        self.check_alive()
-        # update cooldown
-        if self.shoot_cooldown > 0:
-            self.shoot_cooldown -= 1
-
-    def move(self, moving_left, moving_right):
-        # reset movement variables
-        screen_scroll = 0
-        dx = 0
-        dy = 0
-
-        # assign movement variables if moving left or right
-        if moving_left:
-            dx = -self.speed
-            self.flip = True
-            self.direction = -1
-        if moving_right:
-            dx = self.speed
-            self.flip = False
-            self.direction = 1
-##11
-        # jump
-        if self.jump == True and self.in_air == False:
-            self.vel_y = -14
-            self.jump = False
-            self.in_air = True
-
-        # apply gravity
-        self.vel_y += GRAVITY
-        if self.vel_y > 10:
-            self.vel_y
-        dy += self.vel_y
-
-        # check for collision
-        for tile in world.obstacle_list:
-            # check collisions in the x direction
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-                # if the AI has hit a wall then make it turn around
-                if self.char_type == 'enemy':
-                    self.direction *= -1
-                    self.move_counter = 0
-
-            # check for collision in the y direction
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                # check if below the ground, i.e jumping
-                if self.vel_y < 0:
-                    self.vel_y = 0
-                    dy = tile[1].bottom - self.rect.top
-                # check if above the groun, i.e falling
-                elif self.vel_y >= 0:
-                    self.vel_y = 0
-                    self.in_air = False
-                    dy = tile[1].top - self.rect.bottom
-
-        # check for collision with water
-        if pygame.sprite.spritecollide(self, water_group, False):
-            self.health = 0
-
-        # check for collision with exit
-        level_complete = False
-        if pygame.sprite.spritecollide(self, exit_group, False):
-            level_complete = True
-
-        # check if fallen off the map
-        if self.rect.bottom > SCREEN_HEIGHT:
-            self.health = 0
-
-        # check if going off the edges of the screen
-        if self.char_type == 'player':
-            if self.rect.left + dx < 0 or self.rect.right + dx > SCREEN_WIDTH:
-                dx = 0
-
-        # update rectangle position
-        self.rect.x += dx
-        self.rect.y += dy
-
-        # update scroll based on player postion
-        if self.char_type == 'player':
-            if (self.rect.right > SCREEN_WIDTH - SCROLL_THRESH and bg_scroll < (
-                    world.level_length * TILE_SIZE) - SCREEN_WIDTH) or (
-                    self.rect.left < SCROLL_THRESH and bg_scroll > abs(dx)):
-                self.rect.x -= dx
-                screen_scroll = -dx
-
-        return screen_scroll, level_complete
-
-    def shoot(self):
-        if self.shoot_cooldown == 0 and self.ammo > 0:
-            self.shoot_cooldown = 20
-            bullet = Bullet(self.rect.centerx + (0.75 * self.rect.size[0] * self.direction), self.rect.centery,
-                            self.direction)
-            bullet_group.add(bullet)
-            # reduce ammo
-            self.ammo -= 1
-            shot_fx.play()
-
-    def ai(self):
-        if self.alive and player.alive:
-            if self.idling == False and random.randint(1, 200) == 1:
-                self.update_action(0)  # 0: idle
-                self.idling = True
-                self.idling_counter = 50
-            # check if the AI is near the player
-            if self.vision.colliderect(player.rect):
-                # stop running and face the player
-                self.update_action(0)  # 0: idle
-                # shoot
-                self.shoot()
-            else:
-                if self.idling == False:
-                    if self.direction == 1:
-                        ai_moving_right = True
-                    else:
-                        ai_moving_right = False
-                    ai_moving_left = not ai_moving_right
-                    self.move(ai_moving_left, ai_moving_right)
-                    self.update_action(1)  # 1: run
-                    self.move_counter += 1
-                    # update AI vision as the enemy moves
-                    self.vision.center = (self.rect.centerx + 75 * self.direction, self.rect.centery)
-
-                    if self.move_counter > TILE_SIZE:
-                        self.direction *= -1
-                        self.move_counter *= -1
-                else:
-                    self.idling_counter -= 1
-                    if self.idling_counter <= 0:
-                        self.idling = False
-
-        # scroll
-        self.rect.x += screen_scroll
-
-    def update_animation(self):
-        # update animation
-        ANIMATION_COOLDOWN = 100
-        # update image depending on current frame
-        self.image = self.animation_list[self.action][self.frame_index]
-        # check if enough time has passsed since the last update
-        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
-            self.update_time = pygame.time.get_ticks()
-            self.frame_index += 1
-        # if the animation has run out then reset back to start
-        if self.frame_index >= len(self.animation_list[self.action]):
-            if self.action == 3:
-                self.frame_index = len(self.animation_list[self.action]) - 1
-            else:
-                self.frame_index = 0
-
-    def update_action(self, new_action):
-        # check if the new action is different to the previous one
-        if new_action != self.action:
-            self.action = new_action
-            # update the aniamtion settings
-            self.frame_index = 0
-            self.update_time = pygame.time.get_ticks()
-
-    def check_alive(self):
-        if self.health <= 0:
-            self.health = 0
-            self.speed = 0
-            self.alive = False
-            self.update_action(3)
-###IMPLEMENTACION DE LAS NUEVAS FUNCIONES-------------
-    def activate_spinach_explosion(self):
-        self.spinach_explosion.activate()
-
-    def activate_broccoli_shield(self):
-        self.broccoli_shield.activate()
-    def draw(self):
-        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 class World():
     def __init__(self):
@@ -253,10 +30,10 @@ class World():
                         decoration = Decoration(img, x * TILE_SIZE, y * TILE_SIZE)
                         decoration_group.add(decoration)
                     elif tile == 15:  # create player
-                        player = Soldier('player', x * TILE_SIZE, y * TILE_SIZE, 1.65, 5, 20, 5)
+                        player = Soldier('player', x * TILE_SIZE, y * TILE_SIZE, 1.65, 5, 20, 5, world)
                         health_bar = HealthBar(10, 10, player.health, player.health)
                     elif tile == 16:  # create enemies
-                        enemy = Soldier('enemy', x * TILE_SIZE, y * TILE_SIZE, 1.65, 2, 20, 0)
+                        enemy = Soldier('enemy', x * TILE_SIZE, y * TILE_SIZE, 1.65, 2, 20, 0, world)
                         enemy_group.add(enemy)
                     elif tile == 17:  # create ammo box
                         item_box = ItemBox('Ammo', x * TILE_SIZE, y * TILE_SIZE)
@@ -415,12 +192,12 @@ while run:
             screen.blit(grenade_img, (135 + (x * 15), 60))
 
         player.update()
-        player.draw()
+        player.draw(screen)
 
         for enemy in enemy_group:
-            enemy.ai()
+            enemy.ai(player, water_group, exit_group)
             enemy.update()
-            enemy.draw()
+            enemy.draw(screen)
 
         # update and draw groups
         bullet_group.update()
@@ -447,7 +224,7 @@ while run:
         if player.alive:
             # shoot bullets
             if shoot:
-                player.shoot()
+                player.shoot(Bullet, bullet_group)
                 # throw grenades
             elif grenade and grenade_thrown == False and player.grenades > 0:
                 grenade = grenadier.Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction), player.rect.top,
@@ -462,9 +239,9 @@ while run:
                 player.update_action(1)  # 1: run
             else:
                 player.update_action(0)  # 0: idle
-            screen_scroll, level_complete = player.move(moving_left, moving_right)
+            screen_scroll, level_complete = player.move(moving_left, moving_right, water_group, exit_group)
             bg_scroll -= screen_scroll
-            if player.move(moving_left, moving_right):
+            if player.move(moving_left, moving_right, water_group, exit_group):
                 if player.rect.right >= SCREEN_WIDTH:  # Condición para completar el nivel 1
                     level_complete = True
                     level += 1
@@ -529,5 +306,4 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
 #"""
